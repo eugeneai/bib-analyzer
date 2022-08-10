80de5c872484e002335f3d6a70318a49f5dfb8d0
Evgeny Cherkashin <eugeneai@irnok.net>
2022-08-11 02:08:05 +0800

    Added to more tests, the second one does not work.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,7 +2,7 @@
 #from nose.tools import assert_raises, nottest
 
 #@SkipTest
-from bibanalyzer import mark_book_fields
+from bibanalyzer import mark_book_fields, get_book_year, book2authors
 
 
 class TestBasic:
@@ -25,3 +25,16 @@ class TestBasic:
     def test_splitter(self):
         for p in self.parts:
             assert(len(p)>3), len(p)
+
+    def test_finding_athors(self):
+        years = [1998, 1991, 2011]
+        years = [str(y) for y in years]
+        for i, b in enumerate(self.parts):
+            y = get_book_year(b)
+            assert(y == years[i]), "assertion failed year={}".format(y)
+
+    def test_authors(self):
+        for i, b in enumerate(self.parts):
+            aus = book2authors(b)
+            print(aus)
+            assert(len(aus) > 0), "assertion failed authors={}".format(aus)
